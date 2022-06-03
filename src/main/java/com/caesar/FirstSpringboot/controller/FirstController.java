e6709311da9c0d40c48697a34f5bb83546e0d3ef
caesar <joaovictorcsatu@gmail.com>
2022-06-03 08:34:46 -0300

    Initial commit

---
+++ src/main/java/com/caesar/FirstSpringboot/controller/FirstController.java
@@ -0,0 +1,13 @@
+package com.caesar.FirstSpringboot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FirstController {
+
+    @GetMapping("/")
+    public String HelloWord() {
+        return "Testing a get method in Java test";
+    }
+}
---
+++ src/test/java/com/caesar/FirstSpringboot/FirstSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caesar.FirstSpringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
