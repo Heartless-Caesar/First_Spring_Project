255be516904c23a0b76fc462961c65314e7e64ba
caesar <joaovictorcsatu@gmail.com>
2022-06-04 14:33:35 -0300

    Added Logger example and made URL method paths unique

--- src/main/java/com/caesar/FirstSpringboot/controller/DepartmentController.java
+++ src/main/java/com/caesar/FirstSpringboot/controller/DepartmentController.java
@@ -2,9 +2,12 @@ package com.caesar.FirstSpringboot.controller;
 
 import com.caesar.FirstSpringboot.entity.Department;
 import com.caesar.FirstSpringboot.service.DepartmentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -13,8 +16,14 @@ public class DepartmentController {
     @Autowired
     private DepartmentService departmentService;
 
+
+    private final Logger LOGGER = LoggerFactory.getLogger(DepartmentController.class);
+
     @PostMapping("api/create")
-    public Department AddDepartment(@RequestBody Department obj){
+    public Department AddDepartment(@Valid @RequestBody Department obj){
+        //Logger example, this can be used effectively when debugging
+        LOGGER.info("Added logger inside the POST request controller");
+
         return departmentService.AddDepartment(obj);
     }
 
@@ -28,18 +37,18 @@ public class DepartmentController {
         return departmentService.GetSingle(departmentId);
     }
 
-    @DeleteMapping("/api/single/{id}")
+    @DeleteMapping("/api/delete/{id}")
     public String DeleteDept(@PathVariable("id") Long departmentId){
         departmentService.DeleteDept(departmentId);
         return "Deleted";
     }
 
-    @PutMapping("/api/single/{id}")
+    @PutMapping("/api/edit/{id}")
     public Department UpdateDept(@PathVariable("id") Long departmentId, @RequestBody Department obj){
         return departmentService.UpdateDept(departmentId,obj);
     }
 
-    @GetMapping("/api/single/{name}")
+    @GetMapping("/api/specific/{name}")
     public Department findByDepartmentNameIgnoreCase(@PathVariable("name") String deptName){
         return departmentService.findByDepartmentNameIgnoreCase(deptName);
     }
